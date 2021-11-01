3ce2fae1ac60632bdaea9f46cd236dc18f0a47f3
Matt Upchurch <upcheezy@gmail.com>
2021-10-31 21:22:21 -0400

    finished lesson

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict" 
+
+var chalk = require('chalk');
+
+console.log("hello node");
---
+++ scripts/bind-data.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var csvParse = require('csv-parse');
+
+// request first file
+fs.readFile('../data/austin-council-districts.json', 'utf8', (err, geojson) => {
+  
+  if (err) throw err;
+  // nested call for the second (could use Promise or async solution)
+  fs.readFile('../data/austin-traffic-signals.csv', 'utf8', (err, csvString) => {
+    
+    if (err) throw err; // stop the script if error
+
+    // parse the CSV file from text to array of objects
+    csvParse(csvString, {
+      columns: true
+    }, (err, csvData) => {
+      
+      bindData(JSON.parse(geojson), csvData);
+
+    });
+  })
+});
+
+function bindData(geojson, csvData) {
+
+  // loop through the features
+  geojson.features.forEach((feature) => {
+
+    // set a variable to 0
+    let count = 0;
+
+    // loop through the array of CSV data objects
+    csvData.forEach((row) => {
+
+      // if IDs match
+      if (feature.properties.council_di === row.COUNCIL_DISTRICT) {
+        // increment the count for that feature
+        count++
+      }
+
+    });
+
+    // when done looping, add the count as a feature property
+    feature.properties.count = count;
+
+  });
+
+  // done with data bind
+  writeFile(geojson);
+
+}
+
+function writeFile(geojson) {
+
+  fs.writeFile('../data/austin-districts-counts.json', JSON.stringify(geojson), 'utf8', function (err) {
+
+    if (err) throw err;
+
+    console.log('File all done. Great success!');
+  })
+
+}
